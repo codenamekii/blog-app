d1fc9ecdb7e39927be55bc356f5681b398071ccc
hesdin <hesdin01@gmail.com>
2025-01-22 18:58:08 +0800

    update

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,11 +1,12 @@
 @props(['active', 'navigate'])
 
 @php
-    $classes = $active ?? false ? 
-    'inline-flex items-center hover:text-yellow-900 text-sm text-yellow-500' :
-    'inline-flex items-center hover:text-yellow-900 text-sm text-gray-500';
+  $classes =
+      $active ?? false
+          ? 'inline-flex items-center hover:text-yellow-900 text-sm text-yellow-500'
+          : 'inline-flex items-center hover:text-yellow-900 text-sm text-gray-500';
 @endphp
 
-<a {{ $navigate ?? true ? 'wire:navigate' : '' }}   {{ $attributes->merge(['class' => $classes]) }}>
-    {{ $slot }}
+<a target="_blank" {{ $navigate ?? true ? 'wire:navigate' : '' }} {{ $attributes->merge(['class' => $classes]) }}>
+  {{ $slot }}
 </a>
